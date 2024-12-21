e90a8a6fa1e73dc4f3bd482d17bb8372b40de8b7
Vruddhi Shah <vhs180903@gmail.com>
2024-12-21 08:50:52 +0530

    Fixed model output.
    The model was not running smoothly and the output was unclear.

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ tokenizer = pickle.load(open('token.pkl','rb'))
 def predict_word(model,tokenizer,text):
     sequence= tokenizer.texts_to_sequences([text])
     sequence= np.array(sequence)
-    preds = np.argmax(model.predict(sequence))
+    preds = np.argmax(model.predict(sequence),axis=-1)
     predicted_word =""
     for key,value in tokenizer.word_index.items():
         if value == preds:
             predicted_word = key
             break
-        print(predicted_word)
-        return predicted_word
+    print(predicted_word)
+    return predicted_word
 while True:
-    text = input("Enter your line")
+    text = input("Enter your line(type 1 to exit)")
     if text=="1":
         break
     else:
         text=text.split(" ")
         text=text[-3:]
         predict_word(model,tokenizer,text)
+        print(f"Predicted next word: {predict_word}")
+        
