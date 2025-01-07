5ac49b353e4ff4f224e3f812c7b397be8c41ed39
Vruddhi Shah <vhs180903@gmail.com>
2025-01-07 16:20:13 +0530

    Making some final changes in deployment

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
 import numpy as np
+from tensorflow.keras.models import load_model
 import pickle
-import gdown
-
-# Download model and tokenizer files
-url_model = "https://drive.google.com/uc?id=1VmxkGEFA6XQV3guHviwgHlb55EpHa6aG"
-output_model = "next_words.keras"
-output_tokenizer = "token.pkl"
-
-gdown.download(url_model, output_model, quiet=False)
-gdown.download("https://drive.google.com/uc?id=1VmxkGEFA6XQV3guHviwgHlb55EpHa6aG", output_tokenizer, quiet=False)
 
 # Load model and tokenizer
-model = load_model(output_model)
+model = load_model('next_words.keras')
+tokenizer = pickle.load(open('token.pkl', 'rb'))
 
-with open(output_tokenizer, 'rb') as f:
-    tokenizer = pickle.load(f)
-
-# Function to predict the next word
-def predict_word(model, tokenizer, text):
+# Prediction function
+def predict_word(text):
     sequence = tokenizer.texts_to_sequences([text])
     sequence = np.array(sequence)
     preds = np.argmax(model.predict(sequence), axis=-1)
@@ -31,15 +20,17 @@ def predict_word(model, tokenizer, text):
     return predicted_word
 
 # Streamlit UI
-st.title("Next Word Predictor")
-st.write("Enter some text, and I'll predict the next word for you!")
+st.title("Next Word Prediction")
+st.markdown("Enter a sentence or phrase, and I will predict the next word.")
 
-input_text = st.text_input("Enter text:", "")
-if st.button("Predict"):
-    if input_text:
-        text = input_text.split(" ")
-        text = " ".join(text[-3:])  # Use last 3 words for prediction
-        predicted_word = predict_word(model, tokenizer, text)
-        st.success(f"The predicted next word is: **{predicted_word}**")
-    else:
-        st.warning("Please enter some text.")
+user_input = st.text_input("Type your text here:")
+
+if user_input:
+    # Split the text into the last three words (or less)
+    text_input = " ".join(user_input.split()[-3:])
+    
+    # Get the prediction
+    predicted_word = predict_word(text_input)
+    
+    # Display the result
+    st.write(f"Predicted Next Word: **{predicted_word}**")
